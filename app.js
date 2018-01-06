c93ae15186d03d9be7ee1e9361b34be80d176b58
lucian95 <luciancondrea95@gmail.com>
2018-01-06 15:51:14 -0500

    Serve static resources

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req,res) => {
 	res.sendFile(__dirname + '/public/index.html');
 });
