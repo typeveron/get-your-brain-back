759045c39fcc921f81c814d7b0fdd7c10b9c87d6
lucian95 <luciancondrea95@gmail.com>
2018-03-09 16:13:17 -0500

    Remove settings functions from global scope and add settings to localStorage

--- public/settings.js
+++ public/settings.js
@@ -1,78 +1,58 @@
-﻿function showInstructions() {
-    $("#instructions").show();
-    $("#instructions-link").hide();
-}
+(function(window) {
 
-function hideInstructions() {
-    $("#instructions").hide();
-    $("#instructions-link").show();
-}
+    // Check that settings were not previously stored.
+    // localStorage should only contain 3 setting items
+    // and 1 audio array. (Audio loaded after settings)
+    if (localStorage.length === 0) {
+        localStorage.setItem('n-setting', 2);
+        localStorage.setItem('rounds-setting', 15);
+        localStorage.setItem('round-duration-setting', 2000);
 
-function toggleInstructions() {
-	var ins = document.getElementById('instructions');
-	if (ins.hidden)
-		ins.hidden = false;
-	else
-	    ins.hidden = true;
-}
+        document.getElementById('n-setting').value = 2;
+        document.getElementById('rounds-setting').value = 15;
+        document.getElementById('round-duration-setting').value = 2000;
+    } else {
+        let nSetting = localStorage.getItem('n-setting');
+        let roundSetting = localStorage.getItem('rounds-setting');
+        let durationSetting = localStorage.getItem('round-duration-setting');
 
-function showSettings() {
-	document.getElementById('settings-table').hidden = false;
-	document.getElementById('hide-settings-button').hidden = false;
-}
+        document.getElementById('n-setting').value = nSetting;
+        document.getElementById('rounds-setting').value = roundSetting;
+        document.getElementById('round-duration-setting').value = durationSetting;
+    }
 
-function hideSettings() {
-	document.getElementById('settings-table').hidden = true;
-	document.getElementById('hide-settings-button').hidden = true;
-}
+    document.getElementById('instructions-button').onclick = function toggleInstructions() {
+        var ins = document.getElementById('instructions');
+        ins.hidden = ins.hidden ? false : true;
+    };
 
-function setDefaultSettings() {
-	document.getElementById('n-back').value = 2;
-	document.getElementById('rounds').value = 10;
-	document.getElementById('round-duration').value = 2000;
-}
+    document.getElementById('settings-button').onclick = function showSettings() {
+        document.getElementById('settings-table').hidden = false;
+        document.getElementById('hide-settings-button').hidden = false;
+    };
 
-function changeSetting (setting) {
-	if (setting.id === 'n-back') {
-		document.getElementById('setting-added-n').innerText = '+' + setting.value;
-		document.getElementById('header-n').innerText = setting.value;
-		game.maxRounds = game.maxRounds - game.n + Number(setting.value);
-		game.n = Number(setting.value);
-				
-	} else if (setting.id === 'rounds') {
-		game.maxRounds = Number(setting.value) + game.n;
-		
-	} else if (setting.id === 'round-duration') {
-		game.roundDisplayDuration = Number(setting.value);
-		
-	}
-}
+    document.getElementById('hide-settings-button').onclick = function hideSettings(e) {
+        e.cancelBubble = true;
+        document.getElementById('settings-table').hidden = true;
+        document.getElementById('hide-settings-button').hidden = true;
+    };
 
-// TODO: Make game fit the whole screen. Remove footer when in game.
-// make the match 'buttons' reasonably sized and placed.
+    document.getElementById('n-setting').onchange = function changeN(e) {
+        const N = e.target.value;
+        localStorage.setItem('n-setting', N);
 
-//function setGameHeight() {
-//	var height = window.innerHeight;
-//	
-//	
-//}
+        document.getElementById('setting-added-n').innerText = '+' + N;
+        document.getElementById('header-n').innerText = N;
+    };
 
+    document.getElementById('rounds-setting').onchange = function change(e) {
+        const rounds = e.target.value;
+        localStorage.setItem('rounds-setting', rounds);
+    };
 
+    document.getElementById('round-duration-setting').onchange = function changeN(e) {
+        const duration = e.target.value;
+        localStorage.setItem('round-duration-setting', duration);
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})(window)
